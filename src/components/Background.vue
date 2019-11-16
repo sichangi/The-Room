f6fac2cc47848291a198d54248c7809c6fe9b0ad
Mecolela Sichangi <mecolela@gmail.com>
2019-11-16 14:43:34 +0300

    Minor: bump debounce trigger

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -45,7 +45,7 @@
     },
     created() {
       this.instance = new VirtualScroll({multiplier: 5})
-      this.instance.on(debounce(this.triggerScroll, 630))
+      this.instance.on(debounce(this.triggerScroll, 760))
     },
     mounted() {
       this.init()
